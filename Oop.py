fcefbc90e43a08e6a77c49b1ce24a228ea922895
manuellah <emmanumuthui@gmail.com>
2016-11-11 12:32:28 +0300

    successfully implimented my oop task

--- Oop.py
+++ Oop.py
@@ -36,21 +36,34 @@ class FootballTeam(object):
         return self.players
     
 class FootballClub(FootballTeam):
-    def join_new_league('league_name'):
-        pass
+    def join_new_league(league_name):
+        self.leagues.append(self.leagues)
     
-    def remove_league('league_name'):
-        pass
+    def remove_league(league_name):
+        if league_name in self.leagues:
+            self.leagues.remove(league_name)
+            return "{} successfully removed".format(league_name)
+        
+        else:
+            return "{} no such league we have joined".format(league_name)
     
     def current_leagues():
-        pass
+        return self.leagues
     
-    def add_club_sponsor('name'):
-        pass
+    def add_club_sponsor(name):
+        self.sponsors.append(name)
+        return self.sponsors
     
-    def remove_club_sponsor('name'):
-        pass
+    def remove_club_sponsor(name):
+        if name in self.sponsors:
+            self.sponsors.remove(name)
+            return "{} successfully removed".format(name)
+        
+        else:
+            return "{} no such sponsor in our club".format(name)
     
+    def check_club_sponsors():
+        return self.sponsors
     
     
     
