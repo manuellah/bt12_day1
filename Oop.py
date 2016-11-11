f995ac0692f46348d02b625d886fac1b75fb6f70
manuellah <emmanumuthui@gmail.com>
2016-11-11 12:00:16 +0300

    my oop class skeleton done

---
+++ Oop.py
@@ -0,0 +1,38 @@
+class FootballTeam(object):
+    def __init__(self, team_name, home_ground):
+        self.team_name = team_name
+        self.home_ground = home_ground
+        self.players = []
+        self.coach = ''
+        self.captain = ''
+        self.leagues = []
+        self.sponsors = []
+    def add_player('name'):
+        pass
+    
+    def remove_player('name'):
+        pass
+    
+    def set_team_captain('name'): 
+        pass
+        
+    def set_team_coach('name'):
+        pass
+    
+    def change_home_ground():
+        pass
+    
+class FootballClub(FootballTeam):
+    def join_new_league():
+        pass
+    
+    def remove_league():
+        pass
+    
+    def current_leagues():
+        pass
+    
+    def add_club_sponsor():
+        pass
+    
+    def remove_club_sponsor():
